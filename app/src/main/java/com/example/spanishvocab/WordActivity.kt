b955527b99c47153551e7e73f7664f3bbe1663c4
Dick Davis <dick@hey.com>
2022-02-28 01:43:45 -0600

    Implemented word checking functionality

--- app/src/main/java/com/example/spanishvocab/WordActivity.kt
+++ app/src/main/java/com/example/spanishvocab/WordActivity.kt
@@ -2,10 +2,13 @@ package com.example.spanishvocab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.spanishvocab.databinding.ActivityWordBinding
 
-class WordActivity : AppCompatActivity() {
+class WordActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding : ActivityWordBinding
+    lateinit var wordTranslation : WordTranslation
+    private var displayImage : Int = R.drawable.ic_face_thinking_person
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,10 +16,22 @@ class WordActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val intent = getIntent()
-        val wordTranslation = intent.getSerializableExtra(
+        wordTranslation = intent.getSerializableExtra(
             getString(R.string.word_translation_intent_key)
         ) as WordTranslation
 
         binding.spanishWord.setText(wordTranslation.spanishWord)
+        binding.imageView.setImageResource(displayImage)
+        binding.checkWordButton.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        val guess = binding.inputTextview.text.toString()
+        if (guess == wordTranslation.englishWord) {
+            displayImage = R.drawable.ic_teeth_face
+        } else {
+            displayImage = R.drawable.ic_face_disgusting
+        }
+        binding.imageView.setImageResource(displayImage)
     }
 }
