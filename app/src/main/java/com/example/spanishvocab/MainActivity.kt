d7851e8133ca2ee0f0f355c7d8c440a39c5c2567
Dick Davis <dick@hey.com>
2022-02-27 23:31:00 -0600

    Implemented recyclerview with data

--- app/src/main/java/com/example/spanishvocab/MainActivity.kt
+++ app/src/main/java/com/example/spanishvocab/MainActivity.kt
@@ -2,7 +2,15 @@ package com.example.spanishvocab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.spanishvocab.databinding.ActivityMainBinding
+import java.io.InputStream
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +19,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.mainRecyclerview.layoutManager = layoutManager
+        binding.mainRecyclerview.setHasFixedSize(true)
+
+        val adapter = MainAdapter()
+        binding.mainRecyclerview.adapter = adapter
+    }
+
+    inner class MainViewHolder(val view : View) : RecyclerView.ViewHolder(view) {
+        fun setText(text : String) {
+           view.findViewById<TextView>(R.id.item_textview)
+               .setText(text)
+        }
+    }
+
+    inner class MainAdapter() : RecyclerView.Adapter<MainViewHolder>() {
+        var spanishWordList = mutableListOf<String>()
+        var englishWordList = mutableListOf<String>()
+
+        init { readWords() }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_view, parent, false)
+            return MainViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+           holder.setText(spanishWordList[position])
+        }
+
+        override fun getItemCount(): Int {
+            return spanishWordList.size;
+        }
+
+        fun readWords() {
+            val input : InputStream = getAssets().open("spanish_words.txt")
+            val scanner = Scanner(input)
+            while(scanner.hasNextLine()){
+                spanishWordList.add(scanner.nextLine().toString())
+                englishWordList.add(scanner.nextLine().toString())
+                scanner.nextLine()
+            }
+        }
     }
 }
